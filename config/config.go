c91f8a56054f2f7ac816009dd27ece5ca0cd597e
Xylon <x956200361@gmail.com>
2020-10-05 23:54:53 +0800

    update the config

--- config/config.go
+++ config/config.go
@@ -19,38 +19,35 @@ type DataBaseConfig struct {
 }
 
 type MongoConfig struct {
-	DBAddress         string `json:"db_address"`
-	DBName            string `json:"db_name"`
-	TimeOut           int    `json:"time_out"`
-	ClassCollection   string `json:"class_collection"`
-	StudentCollection string `json:"student_collection"`
+	DBAddress              string `json:"db_address"`
+	DBName                 string `json:"db_name"`
+	TimeOut                int    `json:"time_out"`
+	ClassCollection        string `json:"class_collection"`
+	StudentCollection      string `json:"student_collection"`
+	HomeworkInfoCollection string `json:"homework_info_collection"`
 }
 
 //   API interface    //
 
 type APIConfig struct {
-	TeacherHost            string              `json:"teacher_host"`
-	WebsocketPort          string              `json:"websocket_port"`
-	WebsocketCloseDuration int64               `json:"websocket_close_duration"`
-	StudentLogAPI          StudentLogAPI       `json:"student_log_api"`
-	StudentHandUpAPI StudentHandUpAPI `json:"student_hand_up_api"`
+	TeacherHost            string           `json:"teacher_host"`
+	WebsocketPort          string           `json:"websocket_port"`
+	WebsocketCloseDuration int64            `json:"websocket_close_duration"`
+	StudentLogAPI          StudentLogAPI    `json:"student_log_api"`
+	StudentHandUpAPI       StudentHandUpAPI `json:"student_hand_up_api"`
 }
 
 // student log in api for check password
 
 type StudentLogAPI struct {
-	Method             string             `json:"method"`
-	Path               string             `json:"path"`
+	Method string `json:"method"`
+	Path   string `json:"path"`
 }
-
 
 type StudentHandUpAPI struct {
-	Method string	`json:"method"`
-	Path string `json:"path"`
+	Method string `json:"method"`
+	Path   string `json:"path"`
 }
-
-
-
 
 var Config ConfigObj
 
